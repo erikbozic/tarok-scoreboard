08b49f950b7b25f680c38d065436b41f631b5bed
erikbozic <bozic.erik@gmail.com>
2018-03-09 23:21:42 +0100

    'whoopsie

--- src/TarokScoreBoard.Shared/ModifierTypeDbEnum.cs
+++ src/TarokScoreBoard.Shared/ModifierTypeDbEnum.cs
@@ -4,7 +4,7 @@
   {
     public const string TRULA = "trula";
     public const string KRALJI = "kralji";
-    public const string KRALJ_ULTIMO = "'kralj_ultimo";
+    public const string KRALJ_ULTIMO = "kralj_ultimo";
     public const string PAGAT_ULTIMO = "pagat_ultimo";
     public const string BARVNI_VALAT = "barvni_valat";
     public const string VALAT = "valat";
