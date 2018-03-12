9a2e2f1ad2bc40c0c939ab7e52e503ae5edf89d7
erikbozic <bozic.erik@gmail.com>
2018-03-12 07:29:54 +0100

    moves swagger to root, adds to TeamPlayerStatistics

--- src/TarokScoreBoard.Api/Startup.cs
+++ src/TarokScoreBoard.Api/Startup.cs
@@ -77,6 +77,7 @@ namespace TarokScoreBoard.Api
       app.UseSwaggerUI(c =>
       {
         c.SwaggerEndpoint("/api-docs/v1/scoreboard.json", "Tarok Scoreboard API");
+        c.RoutePrefix = "";
       });
 
       DapperMapping.ConfigureColumnMapping();
--- src/TarokScoreBoard.Infrastructure/Repositories/TeamRepository.cs
+++ src/TarokScoreBoard.Infrastructure/Repositories/TeamRepository.cs
@@ -31,9 +31,11 @@ namespace TarokScoreBoard.Infrastructure.Repositories
                    count(prikolca.player_id)        count_prikolica,
                    count(playing.player_id)         count_played,
                    count(lead_lost.player_id)       count_lead_lost,
-                   count(lead_won.player_id)        count_lead_won
+                   count(lead_won.player_id)        count_lead_won,
+                   sum(played_lost.round_score_change) sum_score_lost,
+                   sum(played_won.round_score_change) sum_score_won
                  FROM round_result rr
-                   INNER JOIN team_player tp ON tp.player_id = rr.player_id AND team_id = :teamId
+                   INNER JOIN team_player tp ON tp.player_id = rr.player_id --AND team_id = :teamId
                    LEFT JOIN round r ON rr.round_id = r.round_id
                    LEFT JOIN round_result lead
                      ON r.round_id = lead.round_id AND lead.player_id = r.lead_player_id AND lead.player_id = rr.player_id
@@ -43,13 +45,17 @@ namespace TarokScoreBoard.Infrastructure.Repositories
                    LEFT JOIN round_result playing ON r.round_id = playing.round_id AND
                                                      (playing.player_id = r.supporting_player_id OR
                                                       playing.player_id = r.lead_player_id) AND playing.player_id = rr.player_id
-                   LEFT JOIN round_result lead_lost   ON r.round_id = lead_lost.round_id AND lead_lost.player_id = r.lead_player_id AND lead_lost.player_id = rr.player_id
+                   LEFT JOIN round_result lead_lost ON r.round_id = lead_lost.round_id AND lead_lost.player_id = r.lead_player_id AND lead_lost.player_id = rr.player_id
                       AND lead_lost.round_score_change < 0
                    LEFT JOIN round_result lead_won   ON r.round_id = lead_won.round_id AND lead_won.player_id = r.lead_player_id AND lead_won.player_id = rr.player_id
                       AND lead_won.round_score_change > 0
+                   LEFT JOIN round_result played_won   ON r.round_id = played_won.round_id AND played_won.player_id = rr.player_id
+                      AND played_won.round_score_change > 0
+                   LEFT JOIN round_result played_lost   ON r.round_id = played_lost.round_id AND played_lost.player_id = rr.player_id
+                      AND played_lost.round_score_change < 0
                  GROUP BY rr.player_id
                ) stat
-            LEFT JOIN team_player tp ON tp.player_id = stat.player_id";
+            LEFT JOIN team_player tp ON tp.player_id = stat.player_id;";
 
       return await this.conn.QueryAsync(sql, new { teamId });
     }
